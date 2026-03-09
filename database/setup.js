82700cfa5da5b53659659b7558d5ff324da06149
Aefrly <fraley.allison@ic.edu>
2026-03-09 12:12:02 -0500

    Create a Sequelize instance

--- database/setup.js
+++ database/setup.js
@@ -1,3 +1,9 @@
 // Create database and models
 const { Sequelize, DataTypes } = require("sequelize");
 require('dotenv').config();
+
+const db = new Sequelize({ 
+    dialect: 'sqlite', 
+    storage: `database/${process.env.DB_NAME}` || 'database/library.db', 
+    logging: console.log // Not necessary, but shows SQL queries in the console 
+});
