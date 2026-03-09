475470d6fd27e9212af8431282326d2df70479c3
Aefrly <fraley.allison@ic.edu>
2026-03-09 12:40:17 -0500

    Testing connection and creating the database

--- database/setup.js
+++ database/setup.js
@@ -7,3 +7,17 @@ const db = new Sequelize({
     storage: `database/${process.env.DB_NAME}` || 'database/library.db', 
     logging: console.log // Not necessary, but shows SQL queries in the console 
 });
+
+async function setupDatabase() { 
+    try { 
+        await db.authenticate(); 
+        console.log('Connection to database established successfully.'); 
+
+        await db.sync({ force: true })
+        console.log('Database file created at:',`database/${process.env.DB_NAME}`); 
+
+        await db.close(); 
+    } catch (error) { 
+         console.error('Unable to connect to the database:', error); 
+    } 
+}
