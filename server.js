90b4090767e9390021d0f71585549676cf5e0a6f
Aefrly <fraley.allison@ic.edu>
2026-03-10 08:42:37 -0500

    Created my express server and defined a function to verify the connection to the database

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // Create your Express server that queries the database here
+const express = require('express');
+const { Book } = require('./database/setup.js');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+async function verifyDatabaseConnection() {
+    try {
+        await Book.sequelize.authenticate();
+        console.log('Connection to database established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+// Call the function to verify the database connection when the server starts
+verifyDatabaseConnection();
